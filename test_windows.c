91e054c94daff4d3bb6f56c1ff76bd3ecf1b5497
jukka-pekka.iivonen <wondermacros@yahoo.com>
2020-12-01 21:40:08 +0200

    Move Windows config to own file

--- test_windows.c
+++ test_windows.c
@@ -1,43 +1,4 @@
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <Windows.h>
-
-
-
-static inline bool
-atomic_cas(void** object, void** expected, void* desired)
-{
-    bool success;
-
-    if (sizeof(void*) == 4) {
-        long* o = (long*) object;
-        LONG d = (LONG) desired;
-        long comp = (long) *expected;
-
-        int32_t value = _InterlockedCompareExchange(o, d, comp);
-        success = value == comp;
-        if (!success)
-            *expected = (void*) value;
-    } else {
-        int64_t* o = (int64_t*) object;
-        int64_t d = (int64_t) desired;
-        int64_t comp = (int64_t) *expected;
-
-        int64_t value = _InterlockedCompareExchange64(o, d, comp);
-        success = value == comp;
-        if (!success)
-            *expected = (void*) value;
-    }
-
-    return success;
-}
-
-#define CAS(object, expected, desired)      \
-    atomic_cas(object, expected, desired)
-#define BZERO(ptr,n)                        \
-    SecureZeroMemory(ptr,n)
-
+#include "config_windows.h"
 #include "frame_allocator.h"
 
 
